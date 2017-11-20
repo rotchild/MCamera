0e290db0554b81d267a695e2b66c16f1816f0768
rotchild <2594962259@qq.com>
2017-11-20 09:33:55 +0800

    Initial commit

---
+++ app/src/main/java/com/mlib/cx/mcamera/CameraMainAct.java
@@ -0,0 +1,13 @@
+package com.mlib.cx.mcamera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CameraMainAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_main);
+    }
+}
